23b9c73a5a64f428d0551eb47f819fd879a1471d
chenwenjie <chrischen0405@gmail.com>
2019-02-19 01:05:25 +0800

    新建登录页面

--- page/pages/index/index.js
+++ page/pages/index/index.js
@@ -15,6 +15,11 @@ Page({
       url: '../logs/logs'
     })
   },
+  login: function(){
+    wx.navigateTo({
+      url: '../login/login'
+    })
+  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
