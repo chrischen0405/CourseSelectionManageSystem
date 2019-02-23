42169f712542c903a6b47cc45d6cb8e7deef8474
chenwenjie <chrischen0405@gmail.com>
2019-02-23 22:03:31 +0800

    新建弹窗组件+完成退课弹窗

---
+++ page/component/dialog/dialog.js
@@ -0,0 +1,55 @@
+// component/dialog/dialog.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      // 初始值
+      value: '课程详情'
+    },
+    content: {
+      type: Array
+    },
+    confirmText: {
+      type: String,
+      value: '确定'
+    },
+    cancelText: {
+      type: String,
+      value: '取消'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showDialog: false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    show(){
+      this.setData({
+        showDialog: true
+      })
+    },
+    hide(){
+      this.setData({
+        showDialog: false
+      })
+    },
+    _cancel() {
+      //触发取消回调
+      this.triggerEvent("cancel")
+    },
+    _confirm() {
+      //触发成功回调
+      this.triggerEvent("confirm");
+    }
+  }
+})
--- page/pages/student/myCourse/myCourse.js
+++ page/pages/student/myCourse/myCourse.js
@@ -13,18 +13,11 @@ Component({
   data: {
     colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"],
     courseList: [
-      { "id":0, "xqj": 1, "skjc": 1, "skcd": 3, "kcmc": "高等数学@教A-301" },
-      { "id":1, "xqj": 1, "skjc": 5, "skcd": 3, "kcmc": "高等数学@教A-301" },
-      { "id":2, "xqj": 2, "skjc": 1, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "id":3, "xqj": 2, "skjc": 8, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "id":4, "xqj": 3, "skjc": 4, "skcd": 1, "kcmc": "高等数学@教A-301" },
-      { "id":5, "xqj": 3, "skjc": 8, "skcd": 1, "kcmc": "高等数学@教A-301" },
-      { "id":6, "xqj": 3, "skjc": 5, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "id":7, "xqj": 4, "skjc": 2, "skcd": 3, "kcmc": "高等数学@教A-301" },
-      { "id":8, "xqj": 4, "skjc": 8, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "id":9, "xqj": 5, "skjc": 1, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "id":10, "xqj": 6, "skjc": 3, "skcd": 2, "kcmc": "高等数学@教A-301" },
-      { "id":11, "xqj": 7, "skjc": 5, "skcd": 3, "kcmc": "高等数学@教A-301" },
+      { "id":0, "week": 1, "cstart": 3, "ctime": 3, "cname": "高等数学1", "classroom": "理四222", "teacher":"王老师" },
+      { "id":1, "week": 2, "cstart": 6, "ctime": 3, "cname": "高等数学2", "classroom": "理四224", "teacher":"赵老师" },
+      { "id":2, "week": 3, "cstart": 1, "ctime": 2, "cname": "高等数学3", "classroom": "理四226", "teacher":"钱老师" },
+      { "id":3, "week": 4, "cstart": 10, "ctime": 2, "cname": "高等数学4", "classroom": "理四223", "teacher":"孙老师" },
+      { "id":4, "week": 5, "cstart": 4, "ctime": 1, "cname": "高等数学5", "classroom": "理四225", "teacher":"李老师" },
     ]
   },
 
@@ -33,7 +26,43 @@ Component({
    */
   methods: {
     showCardView:function(e){
-      console.log(e);
-    }
+      let num=e.currentTarget.dataset.index;
+      this.dialog = this.selectComponent("#dialog");
+      this.tkdialog = this.selectComponent("#tkdialog");
+      const data=this.data;
+      const dialogContent=[{
+        label:'课程名称',
+        value:data.courseList[num].cname
+      },
+      {
+        label: '教室',
+        value: data.courseList[num].classroom
+      },
+      {
+        label: '老师',
+        value: data.courseList[num].teacher
+      }];
+      this.setData({
+        dialogContent: dialogContent
+      })
+      this.dialog.show();
+      
+      console.log(num);
+    },
+    // 点击了弹出框的取消
+    handleCancelDialog() {
+      this.dialog.hide()
+    },
+    // 点击了弹出框的确认
+    handleConfirmDialog() {
+      this.dialog.hide();
+      this.tkdialog.show();
+    },
+    handleCancelDialog2() {
+      this.tkdialog.hide();
+    },
+    handleConfirmDialog2() {
+      this.tkdialog.hide();
+    },
   }
 })
