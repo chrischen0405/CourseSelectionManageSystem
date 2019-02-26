edec1907da77b3033a3fbea23b966a896e950f78
chenwenjie <chrischen0405@gmail.com>
2019-02-27 01:41:09 +0800

    学生选课列表弹窗+学生选课搜索框界面

--- page/pages/student/allCourse/allCourse.js
+++ page/pages/student/allCourse/allCourse.js
@@ -30,7 +30,52 @@ Component({
    */
   methods: {
     clickCell(e) {
-      console.log(e);
-    }
+      let num = e.currentTarget.dataset.index;
+      console.log(num);
+      this.dialog = this.selectComponent("#dialog");
+      const data = this.data;
+      const dialogContent = [
+      {
+        label: '课程编号',
+        value: data.courseList[num].id
+      },
+      {
+        label: '课程名称',
+        value: data.courseList[num].cname
+      },
+      {
+        label: '教室',
+        value: data.courseList[num].classroom
+      },
+      {
+        label: '课程名称',
+        value: data.courseList[num].cname
+      },
+      {
+        label: '上课时间',
+        value: '星期' + data.courseList[num].week + ' 第' + data.courseList[num].cstart+'节课'
+      },
+      {
+        label: '课时',
+        value: data.courseList[num].ctime+'课时'
+      },
+      {
+        label: '教师',
+        value: data.courseList[num].teacher
+      }];
+      this.setData({
+        dialogContent: dialogContent
+      })
+      this.dialog.show();
+    },
+    // 点击了弹出框的取消
+    handleCancelDialog() {
+      this.dialog.hide()
+    },
+    // 点击了弹出框的确认
+    handleConfirmDialog() {
+      this.dialog.hide();
+      console.log('ok');
+    },
   }
 })
